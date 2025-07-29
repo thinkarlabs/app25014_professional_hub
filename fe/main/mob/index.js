416d1c19c7dc0a3def660a428b37d0b93050e8f6
Neha Singh <96856900+Neha21singh@users.noreply.github.com>
2025-07-29 19:51:17 +0530

    gg

--- fe/main/mob/index.js
+++ fe/main/mob/index.js
@@ -8,7 +8,3 @@ _app.curr_ses = JSON.parse(sessionStorage.getItem('session_info'));
 _app.load_page('mob.request_lst');
 
  history.replaceState({}, null, "/neev/mob/");
-=======
- history.replaceState({}, null, "/neev/mob");
->>>>>>> f54881dfa605afc8e7df0d28d35756188ffff844
-
